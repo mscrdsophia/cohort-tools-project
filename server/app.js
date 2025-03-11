328bb8ec21b94e403fa97bf45002b97f3b37bec9
Maria sophia Mascardo <mariasophiamascardo@Pias-MacBook-Air.local>
2025-03-11 20:34:15 +0100

    day 5 done

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const cookieParser = require("cookie-parser");
 const PORT = 5005;
 const Student = require("./models/Students.model");
 const Cohort = require("./models/Cohorts.model");
+const User = require("./models/User.model");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -13,8 +14,9 @@ const Cohort = require("./models/Cohorts.model");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
+const { isAuthenticated } = require("./middlewares");
 // MIDDLEWARE
+// require("./config")(app);
 // Research Team - Set up CORS middleware here:
 // ...
 app.use(express.json());
@@ -22,6 +24,10 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+const authRoutes = require("./routes/auth.routes");
+app.use("/auth", authRoutes);
+
+
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -178,6 +184,20 @@ app.delete("/api/cohorts/:cohortId", async (req, res) => {
   try {
     const cohort = await Cohort.findByIdAndDelete(req.params.cohortId);
     res.json(cohort);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.get("/api/users/:id", isAuthenticated, async (req, res) => {
+  try {
+    // user {} or null
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    } else {
+      res.json(user);
+    }
   } catch (error) {
     console.error(error);
   }
---
+++ server/routes/auth.routes.js
@@ -0,0 +1,125 @@
+
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User.model");
+const router = require("express").Router();
+
+const { isAuthenticated } = require("../middlewares");
+
+const saltRounds = 10;
+
+router.post("/signup", (req, res, next) => {
+    const { email, password, name, role } = req.body;
+ 
+  // Check if the email or password or name is provided as an empty string 
+  if (email === '' || password === '' || name === '') {
+    res.status(400).json({ message: "Provide email, password and name" });
+    return;
+  }
+ 
+  // Use regex to validate the email format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+  if (!emailRegex.test(email)) {
+    res.status(400).json({ message: 'Provide a valid email address.' });
+    return;
+  }
+  
+  // Use regex to validate the password format
+  const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!passwordRegex.test(password)) {
+    res.status(400).json({ message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.' });
+    return;
+  }
+ 
+ 
+  // Check the users collection if a user with the same email already exists
+  User.findOne({ email })
+    .then((foundUser) => {
+      // If the user with the same email already exists, send an error response
+      if (foundUser) {
+        res.status(400).json({ message: "User already exists." });
+        return;
+      }
+ 
+      // If the email is unique, proceed to hash the password
+      const salt = bcrypt.genSaltSync(saltRounds); // 4898[yf9wh2b[ebf2f0fh]]
+      const hashedPassword = bcrypt.hashSync(password, salt); // 4898[yf9wh2b[ebf2f0fh]]ugciuurybliuebfaybdlieuyb
+ 
+      // Create a new user in the database
+      // We return a pending promise, which allows us to chain another `then` 
+      return User.create({ email, password: hashedPassword, name, role });
+    })
+    .then((createdUser) => {
+      // Deconstruct the newly created user object to omit the password
+      // We should never expose passwords publicly
+      const { email, name, _id, role } = createdUser;
+    
+      // Create a new object that doesn't expose the password
+      const user = { email, name, _id, role };
+ 
+      // Send a json response containing the user object
+      res.status(201).json({ user: user });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Internal Server Error" })
+    });
+});
+
+router.post("/login", (req, res, next) => {
+  const { email, password } = req.body;
+ 
+  // Check if email or password are provided as empty string 
+  if (email === '' || password === '') {
+    res.status(400).json({ message: "Provide email and password." });
+    return;
+  }
+ 
+  // Check the users collection if a user with the same email exists
+  User.findOne({ email })
+    .then((foundUser) => {
+    console.log("foundUser",foundUser);
+      if (!foundUser) {
+        // If the user is not found, send an error response
+        res.status(401).json({ message: "User not found." })
+        return;
+      }
+ 
+      // Compare the provided password with the one saved in the database
+      const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
+      console.log("passwordCorrect",passwordCorrect);
+      if (passwordCorrect) {
+        // Deconstruct the user object to omit the password
+        const { _id, email, name  } = foundUser;
+        
+        // Create an object that will be set as the token payload
+        const payload = { _id, email, name };
+ 
+        // Create and sign the token
+        const authToken = jwt.sign( 
+          payload,
+          process.env.TOKEN_SECRET,
+          { algorithm: 'HS256', expiresIn: "6h" }
+        );
+        console.log("authToken",authToken);
+        // Send the token as the response
+        res.status(200).json({ authToken: authToken });
+      }
+      else {
+        res.status(401).json({ message: "Unable to authenticate the user" });
+      }
+ 
+    })
+    .catch(err => res.status(500).json({ message: "Internal Server Error" }));
+});
+
+router.get("/verify", isAuthenticated, (req, res, next) => {
+  console.log("req.payload", req.payload);
+  res.status(200).json(req.payload);
+})
+    // Get the token string from the authorization
+
+   
+
+
+module.exports = router;
